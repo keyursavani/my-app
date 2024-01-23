b20b5e95740fe270bb74bb064424f93c92520e3c
keyur savani <savanikeyur1011@gmail.com>
2024-01-23 09:41:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,16 @@ import React, { useState } from "react";
 function App() {
   const [mode, setMode] = useState('light') // Whether dark mode is enable or not 
   const [alert, setAlert] = useState(null)
-  const toggleMode = () => {
+  const removeBodyClasses = ()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-success')
+  }
+  const toggleMode = (cls) => {
+    removeBodyClasses();
+    document.body.classList.add('bg-'+cls);
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,9 +30,8 @@ export default function TextForm(props) {
     }
 
     const handelCopyClick = ()=>{
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        navigator.clipboard.writeText(text);
+        document.getSelection().removeAllRanges();
         props.showAlert("copy text","success")
     }
 
@@ -55,18 +54,22 @@ export default function TextForm(props) {
             <label for="myBox" className="form-label">Example textarea</label>
             <textarea className="form-control" value={text} onChange={handelOnChange} style={{backgroundColor: props.mode === 'dark'?'grey':'white' , color: props.mode === 'dark' ? 'white' : '#042743'}} id="myBox" rows="8"></textarea>
         </div>
-            <button className="btn btn-primary mx-2" onClick={handelUpClick}>Convert to UpperCase</button>
-            <button className="btn btn-primary mx-2" onClick={handelLoClick}>Convert to LowerCase</button>
-            <button className="btn btn-primary mx-2" onClick={handelClearClick}>Clear the text</button>
-            <button className="btn btn-primary mx-2" onClick={handelCopyClick}>Copy Text</button>
-            <button className="btn btn-primary mx-2" onClick={handelExtraSpaceClick}>Handel Extra Spaces</button>
+            <button disabled={text.length === 0} className="btn btn-primary mx-2 my-2" onClick={handelUpClick}>Convert to UpperCase</button>
+            <button disabled={text.length === 0}  className="btn btn-primary mx-2 my-2" onClick={handelLoClick}>Convert to LowerCase</button>
+            <button disabled={text.length === 0}  className="btn btn-primary mx-2 my-2" onClick={handelClearClick}>Clear the text</button>
+            <button disabled={text.length === 0}  className="btn btn-primary mx-2 my-2" onClick={handelCopyClick}>Copy Text</button>
+            <button disabled={text.length === 0}  className="btn btn-primary mx-2 my-2" onClick={handelExtraSpaceClick}>Handel Extra Spaces</button>
         </div>
         <div className="container my-3" style={{color: props.mode === 'dark'?'white':'#042743'}}>
             <h1>Your Text Summary</h1>
-            <p>{text.split(" ").length} Word and {text.length} Charecters</p>
-            <p>{0.008 * text.split(" ").length} Minuts to read this paragraph</p>
+            <p>{text.split(/\s+/).filter((element)=>{
+                return element.length !== 0; 
+            }).length} Word and {text.length} Charecters</p>
+            <p>{0.008 * text.split(" ").filter((element)=>{
+                return element.length !== 0; 
+            }).length} Minuts to read this paragraph</p>
             <h2>Preview</h2>
-            <p>{text.length > 0 ? text : "Enter something in above textfield"}</p>
+            <p>{text.length > 0 ? text : "Nothing to preview"}</p>
         </div>
         </>
     )
